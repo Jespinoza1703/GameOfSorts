344e3cd1b47f82cee20561922a08fc7e31c7063b
[Fabian] <[ramirezfabian003@gmail.com]>
2018-10-11 06:44:34 -0600

    Created dragon class and nameGenerator class. Create array List with name and last names

---
+++ src/game/logic/entities/dragon.java
@@ -0,0 +1,96 @@
+package game.logic.entities;
+
+import game.logic.trees.TreeNode;
+
+public class dragon {
+
+    private String name;
+    private int reloadSpeed;
+    private int age;
+    private int toughness;
+    private TreeNode father;
+    //falta la clase
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getReloadSpeed() {
+        return reloadSpeed;
+    }
+
+    public void setReloadSpeed(int reloadSpeed) {
+        this.reloadSpeed = reloadSpeed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getToughness() {
+        return toughness;
+    }
+
+    public void setToughness(int toughness) {
+        this.toughness = toughness;
+    }
+
+    public TreeNode getFather() {
+        return father;
+    }
+
+    public void setFather(TreeNode father) {
+        this.father = father;
+    }
+}
