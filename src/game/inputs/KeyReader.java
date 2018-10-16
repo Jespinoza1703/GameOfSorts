efa803a4bf99d13cf94c3515aab8f5326c5fd101
JoDaniel1412 <jodaniel1412@gmail.com>
2018-10-15 18:20:57 -0600

    Added JFoenix library Added KeyReader class

---
+++ src/game/inputs/KeyReader.java
@@ -0,0 +1,17 @@
+package game.inputs;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class KeyReader extends KeyAdapter {
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        super.keyPressed(e);
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        super.keyReleased(e);
+    }
+}
