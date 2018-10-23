46f7f1c85d9f083f3cbd999a1a2d2688a0690903
[Fabian] <[ramirezfabian003@gmail.com]>
2018-10-23 12:30:39 -0600

    Fixed type bugs

--- src/game/entities/Dragon.java
+++ src/game/entities/Dragon.java
@@ -1,65 +1,52 @@
 package game.entities;
 
-import game.logic.lists.Node;
-import game.logic.lists.SimpleList;
-import game.logic.sorts.SortMethods;
-import game.logic.trees.AVLTree;
-import game.logic.trees.BinaryTree;
-import javafx.scene.shape.Rectangle;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import game.GameController;
+import game.draw.Drawer;
+import game.draw.Sprite;
+import game.event.handler.inputs.Collisions;
+import game.event.handler.inputs.KeyReader;
+import util.Clock;
 import util.NameGenerator;
+
+import java.util.ArrayList;
 
 public class Dragon extends Entity {
 
     private String name;
     private Dragon parent;
-    private int lives; // [1, 3]
-    private int fire_rate;  // [10, 100]
+    private double lives; // [1, 3]
+    private double fire_rate = (((Math.random())*15000));  // [10, 100]
     private int age;  // [1, 1000]
     private String rank;  // Commander / Captain / Infantry
-    private int xPoss;
-    private int xSpeed;
-    private static SimpleList dragonsList = new SimpleList(); //Contains the alive dragons list
-    private static AVLTree dragonOrganization = new AVLTree();
-    private static BinaryTree BinaryDragon = new BinaryTree();
-    private static SimpleList DragonListSorted = new SimpleList();
-
-    private static Logger logger = LoggerFactory.getLogger("Dragon");
-
-
-
-
-    public Dragon () {
-
-        logger.debug("Created new Dragon");
-        int lives = ((int)((Math.random())*100));
-        this.name = (NameGenerator.generateName());
-        logger.debug(this + " Name: " + this.name);
-        this.fire_rate = ((int)((Math.random())*100));
-        logger.debug(this + " Fire Rate: " + this.fire_rate);
-        this.age = ((int)((Math.random())*1000));
-        logger.debug(this + " Age: " + this.age);
-        if (lives <= 33){
-            this.setLives(1);
-            logger.debug(this + " Lives: " + this.lives);
-        }else if(lives <= 66){
-            this.setLives(2);
-            logger.debug(this + " Lives: " + this.lives);
-        }else if(lives < 100){
-            this.setLives(3);
-            logger.debug(this + " Lives: " + this.lives);
-        }
-        dragonsList.addDragon(this);
+    private double xPoss, yPoss;
+    private double dragonWidth = 80, dragonHeight = 65;
+    private double xSpeed = 1;
+    private long lastTime = 0;
+    private KeyReader key;
+    private Clock clock = Clock.getInstance();
+    private Sprite sprite;
+    private ArrayList<Sprite> movementAnimations = new ArrayList<>();
+    private double animationTimer = 200;
+    private double lastAnimationTime = 0;
+    private int currentSprite = 0;
 
 
+    public Dragon (double xPoss, double yPoss) {
+        this.xPoss = xPoss;
+        this.yPoss = yPoss;
+        sprite = loadImages();
+        Drawer.getInstance().addDrawAtEnd(this);
+        GameController.getInstance().addEntity(this);
+        Collisions.getInstance().addDragon(this);
+    }
+
+    public Dragon(){
 
     }
 
     public Dragon(Dragon parent, int lives, int age, String rank) {
-        logger.debug("Created new Dragon");
         this.name = (NameGenerator.generateName());
-        this.fire_rate = ((int)((Math.random())*100));
+        this.fire_rate = (((Math.random())*100));
         this.age = age;
         this.parent = parent;
         this.lives = lives;
@@ -68,63 +55,82 @@ public class Dragon extends Entity {
 
     @Override
     public void update(){
-        xPoss -= xSpeed;
+        move();
+
+        if (canShoot()){
+            shoot();
+        }
     }
 
     @Override
-    public Rectangle draw() {
-        return null;
+    public void destroy() {
+        Drawer.getInstance().deleteEntity(this);
+        GameController.getInstance().deleteEntity(this);
+        Collisions.getInstance().deleteDragon(this);
     }
 
+    @Override
+    public Sprite getSprite() {
+        return sprite;
+    }
+
+    @Override
+    public Sprite draw() {
+        long time = clock.getTime();
+        if (time - lastAnimationTime > animationTimer){
+            sprite = movementAnimations.get(currentSprite);
+            lastAnimationTime = time;
+            currentSprite = (currentSprite + 1) % movementAnimations.size();
+        }
+        sprite.move(xPoss, yPoss);
+        return sprite;
+    }
+
+    /**
+     * Loads images for dragon
+     * @return Sprite
+     */
+    private Sprite loadImages(){
+        movementAnimations.add(sprite = new Sprite(xPoss, yPoss, dragonWidth, dragonHeight,
+                "file:res/img/entities/dragon/dMovement2"));
+        movementAnimations.add(sprite = new Sprite(xPoss, yPoss, dragonWidth, dragonHeight,
+                "file:res/img/entities/dragon/dMovement1"));
+        movementAnimations.add(sprite = new Sprite(xPoss, yPoss, dragonWidth, dragonHeight,
+                "file:res/img/entities/dragon/dMovement3"));
+        return movementAnimations.get(0);
+
+    }
     private void hit(){
-        logger.info(this + " has been hit it ");
 
     }
 
     private void shoot(){
-        logger.debug(this + " has shoot");
+        FireBall fireBall = new FireBall(xPoss, yPoss, 35, 35,-1, 0);
+        Collisions.getInstance().addDragonBullets(fireBall);
     }
 
-    public void dies(){
-        logger.info(this + " has been eliminated");
-        int i = 0;
-        while (dragonsList.getByIndex(i).getDragon() != this && dragonsList.getByIndex(i).getNext() != null){
-            i++;
-        }
-        dragonsList.delete(i);
+    private void dies(){
+
     }
 
     private void pressed(){
-        logger.debug(this + "has been clicked");
+
     }
 
+    private void move(){
+        xPoss -= xSpeed;
 
-
-
-
-    private static SimpleList getSortedListByAge(){
-        logger.debug("Change Dragons organization to Sorted by Age");
-        updateSortedList();
-        SortMethods.selectionSort(DragonListSorted);
-        return DragonListSorted;
-
-}
-
-    private static SimpleList getSortListByFireRate(){
-        logger.debug("Change Dragons organization to Sorted by Fire Rate");
-        updateSortedList();
-        SortMethods.insertionSort(DragonListSorted);
-        return DragonListSorted;
     }
 
-    private static void updateSortedList(){
-        logger.debug("Update Simple List of dragons that will be sorted");
-        Node temp = dragonsList.getFirst();
-        DragonListSorted.clearOut();
-        while (temp != null){
-            DragonListSorted.addAtEnd(temp.getDragon());
-            temp =  temp.getNext();
+    private Boolean canShoot(){
+        Boolean result = false;
+        long time = clock.getTime();
+        if (time - lastTime > fire_rate){
+            result = true;
+            lastTime = time;
         }
+        return result;
+
     }
 
     /** Getters andSetters **/
@@ -145,19 +151,19 @@ public class Dragon extends Entity {
         this.parent = parent;
     }
 
-    public int getLives() {
+    public double getLives() {
         return lives;
     }
 
-    public void setLives(int lives) {
+    public void setLives(double lives) {
         this.lives = lives;
     }
 
-    public int getFire_rate() {
+    public double getFire_rate() {
         return fire_rate;
     }
 
-    public void setFire_rate(int fire_rate) {
+    public void setFire_rate(double fire_rate) {
         this.fire_rate = fire_rate;
     }
 
@@ -177,51 +183,19 @@ public class Dragon extends Entity {
         this.rank = rank;
     }
 
-    public int getxPoss() {
+    public double getxPoss() {
         return xPoss;
     }
 
-    public void setxPoss(int xPoss) {
+    public void setxPoss(double xPoss) {
         this.xPoss = xPoss;
     }
 
-    public int getxSpeed() {
-        return xSpeed;
+    public double getyPoss() {
+        return yPoss;
     }
 
-    public void setxSpeed(int xSpeed) {
-        this.xSpeed = xSpeed;
-    }
-
-    public static SimpleList getDragonsList() {
-        return dragonsList;
-    }
-
-    public static void setDragonsList(SimpleList dragonsList) {
-        Dragon.dragonsList = dragonsList;
-    }
-
-    public static AVLTree getDragonOrganization() {
-        return dragonOrganization;
-    }
-
-    public static void setDragonOrganization(AVLTree dragonOrganization) {
-        Dragon.dragonOrganization = dragonOrganization;
-    }
-
-    public static BinaryTree getBinaryDragon() {
-        return BinaryDragon;
-    }
-
-    public static void setBinaryDragon(BinaryTree binaryDragon) {
-        BinaryDragon = binaryDragon;
-    }
-
-    public static SimpleList getDragonListSorted() {
-        return DragonListSorted;
-    }
-
-    public static void setDragonListSorted(SimpleList dragonListSorted) {
-        DragonListSorted = dragonListSorted;
+    public void setyPoss(double yPoss) {
+        this.yPoss = yPoss;
     }
 }
