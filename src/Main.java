78d14d2cfdb6c111df2e11cc2dffa8a1846f9554
JoDaniel1412 <jodaniel1409@gmail.com>
2018-10-28 14:49:47 -0600

    Added logger to Main.class

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,8 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
+import org.slf4j.MarkerFactory;
 
 import java.io.IOException;
 
@@ -17,19 +19,22 @@ public class Main extends Application {
 
     private static Stage window;
     private static Logger logger = LoggerFactory.getLogger(Main.class);
+    private static final Marker GUI = MarkerFactory.getMarker("GUI");
+    private static final Marker SYS = MarkerFactory.getMarker("SYS");
 
     public static void main(String[] args) {
-        logger.debug("App launch");
+        logger.info(SYS, "Running JavaFx Application");
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) {
         window = primaryStage;
         window.setResizable(false);
-        logger.debug("Window not resizable set it");
+        logger.info(GUI, "Window resizable set: false");
         window.setWidth(sScene.getWidth());
         window.setHeight(sScene.getHeight());
+        logger.info(GUI, "Window dimensions set: " + (int)window.getWidth() + "x" + + (int)window.getHeight());
         window.setTitle("GameOfSorts");
         window.getIcons().add(new Image("file:res/img/icon.png"));
         Interface.init(this);
@@ -40,21 +45,25 @@ public class Main extends Application {
 
     /**
      * Invokable method to set new Scenes in the window
-     * @throws IOException in case the FXML readable is invalid
      */
-    public static void switchScene() throws IOException {
-        logger.debug("Scene has been swathed");
-        Parent root = FXMLLoader.load(Main.class.getResource(Interface.getScene()));
-        Scene scene = new Scene(root);
-        KeyReader.getInstance().setScene(scene);
-        window.setScene(scene);
+    public static void switchScene() {
+        try {
+            Parent root = FXMLLoader.load(Main.class.getResource(Interface.getScene()));
+            Scene scene = new Scene(root);
+            KeyReader.getInstance().setScene(scene);
+            window.setScene(scene);
+            logger.info(GUI, "New Scene has been set: " + scene.getRoot().getId());
+        } catch (IOException e) {
+            logger.error(GUI, "Failed to load fxml resource" + e);
+        }
+
     }
 
     /**
      * Invokable method to kill all the threads
      */
     public static void close() {
-        logger.debug("All threads has been killed");
+        logger.info(SYS, "Program closed");
         window.close();
         Platform.exit();
         System.exit(0);
