5c8210c7b56a17f41a441c78ea2a66fecd91e3ae
JoDaniel1412 <jodaniel1409@gmail.com>
2018-10-28 20:45:46 -0600

    Added temporal background to sMenu

--- src/graphics/controllers/sMenu.java
+++ src/graphics/controllers/sMenu.java
@@ -1,14 +1,23 @@
 package graphics.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
 
 import java.io.IOException;
 
 public class sMenu extends sScene{
 
+    @FXML
+    AnchorPane anchorPane;
+
     @Override
     protected void initialize() throws IOException {
-
+        Image img = new Image("file:res/img/backgrounds/bg2.jpg");
+        BackgroundSize size = new BackgroundSize(sScene.getWidth(), sScene.getHeight(), false, false, false, false);
+        BackgroundImage bgIMG = new BackgroundImage(img, null, null, BackgroundPosition.CENTER, size);
+        Background bg = new Background(bgIMG);
+        anchorPane.setBackground(bg);
     }
 
     @Override
